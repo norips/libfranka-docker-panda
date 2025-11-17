ee595037962474b77e6b55201e9f875aeb84feff
Raphael Druon <raphdruon@gmail.com>
2025-11-17 13:25:48 +0900

    Update install franka ros2

--- docker_launch_files/franka_ros2/install_franka_ros2.sh
+++ docker_launch_files/franka_ros2/install_franka_ros2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### LIBFRANKA
-cd /docker_volume/libfranka_v0.13.2
+cd /docker_volume/libfranka_v0.9.2
 
 # make sure that libfranka is checked out to the compatible branch (required 0.13.3 as of 08/2024)
 # see https://frankaemika.github.io/docs/compatibility.html
@@ -20,7 +20,7 @@ source /opt/ros/humble/setup.sh
 cd /docker_volume/ros2_ws
 if [ ! -d "install" ]
 then 
-    colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release -DFranka_DIR:PATH=/docker_volume/libfranka_v0.13.2/build
+    colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release -DFranka_DIR:PATH=/docker_volume/libfranka_v0.9.2/build
 fi
 source install/setup.sh
 
